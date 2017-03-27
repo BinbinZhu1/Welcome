4699179f4394d75ffc9579f61400c025de728b97
BinbinZhu1 <hpuzhubin@163.com>
2017-03-27 10:29:52 +0800

    I load a file

---
+++ Factor_decompose.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 16:55:05 2017
+
+@author: welcome
+"""
+
+#一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.
+#编程找出1000以内的所有完数。
+#分析：请参照程序Python 练习实例14。
+   
+def Factor(n):
+    if not isinstance(n,int) or n<0:
+        print "Data error!"
+    elif n==1:
+        print "{}".format(n),
+    factor=[1]
+    for i in range(2,n):
+        if n%i==0:
+            factor.append(i)
+    return factor
+    
+            
+for n in range(2,1001):
+    factor=Factor(n)
+    if n==reduce(lambda x,y:x+y,factor):
+        print n
+        print factor
+#质因数分解
+def decompos(n):
+
+    if not isinstance(n,int) or n<0:
+        print "Data error!"
+    elif n==1:
+        print '{}'.format(n),
+    prime=[1]
+    while n!=1:
+        for i in xrange(2,n+1):
+            if n % i==0:
+                n=n/i
+                if n==1:
+                    prime.append(i)  
+                else:
+                    prime.append(i)
+                break 
+    return prime
+        
+        
+    
+
